826a71e5cc13c7d714fa11bb0d08f7d96e73dad4
yongri910 <yongri910@gmail.com>
2022-11-24 01:34:04 +0800

    create Calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Calendar from './components/Calendar';
 
 function App() {
   return (
     <div className='text-red-500'>
-      welcome
+      <Calendar />
     </div>
   );
 }
