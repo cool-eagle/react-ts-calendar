a232b950d8389568a979dad970435dd07fac949f
yongri910 <yongri910@gmail.com>
2022-11-24 02:14:44 +0800

    Calendar: finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from './components/Calendar';
 
 function App() {
+  const [updatedDate, setUpdatedDate] = useState<Date>(new Date());
+  const onClickWhen = (date: Date) => {
+    setUpdatedDate(date);
+  }
   return (
     <div className='text-red-500'>
-      <Calendar />
+      <Calendar selectedDate={new Date()} onSelectDate={onClickWhen} />
     </div>
   );
 }
